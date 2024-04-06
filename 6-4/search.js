d12901b4b0ccbd22968f8c2917e91e97814211c2
Ксюша Лягашкина <xl@192.168.1.3>
2024-04-06 19:14:01 +0300

    do 6-4

--- 6-4/search.js
+++ 6-4/search.js
@@ -1,3 +1,23 @@
 function search() {
-   // TODO
+    const towns = document.getElementById('towns');
+    const searchText = document.getElementById('searchText').value.trim().toLowerCase();
+    const res = document.getElementById('result');
+
+    let math = 0
+    for (let i = 0; i < towns.children.length; i++) {
+        const town = towns.children[i]
+        const townSea = towns.children[i].textContent.trim().toLowerCase()
+
+        if (townSea.includes(searchText)) {
+            town.style.fontWeight = 'bold'
+            town.style.textDecoration = 'underline'
+
+            math++
+        } else {
+            town.style.fontWeight = 'normal'
+            town.style.textDecoration = 'none'
+        }
+
+    }
+    res.textContent = `${math} found`
 }
